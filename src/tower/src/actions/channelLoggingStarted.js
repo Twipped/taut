ebf4f4724f991a0e96b2bf66c35a6a93a2f46020
ChiperSoft <chiper@chipersoft.com>
2015-10-11 08:23:58 -0700

    Fix logging events not showing up on the channel history

--- src/tower/src/actions/channelLoggingStarted.js
+++ src/tower/src/actions/channelLoggingStarted.js
@@ -2,8 +2,9 @@
 var mq  = require('taut.shared/io/mq');
 
 module.exports = function (target) {
-	return mq.emit('irc:incoming', 'public', 'logging:started', target, {
+	return mq.emit('irc:incoming', 'public', 'logging:started', target.toLowerCase(), {
 		event: 'logging:started',
+		target: target.toLowerCase(),
 		userid: 'TOWER',
 		connid: 'TOWER',
 		timestamp: Date.now()
--- src/tower/src/actions/channelLoggingStopped.js
+++ src/tower/src/actions/channelLoggingStopped.js
@@ -2,8 +2,9 @@
 var mq  = require('taut.shared/io/mq');
 
 module.exports = function (target) {
-	return mq.emit('irc:incoming', 'public', 'logging:halted', target, {
+	return mq.emit('irc:incoming', 'public', 'logging:halted', target.toLowerCase(), {
 		event: 'logging:halted',
+		target: target.toLowerCase(),
 		userid: 'TOWER',
 		connid: 'TOWER',
 		timestamp: Date.now()
