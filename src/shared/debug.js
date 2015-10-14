b96a47e44985eb58ecf2d246521daccc8adfbd96
ChiperSoft <chiper@chipersoft.com>
2015-10-14 13:51:52 -0700

    FFS, I thought I fixed that!

--- src/shared/debug.js
+++ src/shared/debug.js
@@ -18,7 +18,7 @@ function getDebug (name) {
 
 	if (!debugCache[name]) {
 		var log = debug(config.name + (name && ':' + name || ''));
-		log.log = console.error.bind(console); // eslint-disable-line no-console
+		log.log = console.log.bind(console); // eslint-disable-line no-console
 		var error = debug(config.name + (name && ':' + name || '') + ':error');
 		error.log = console.error.bind(console); // eslint-disable-line no-console
 
