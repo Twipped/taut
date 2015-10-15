164742879b735ffddf214ae4f048e9a57db731c1
ChiperSoft <chiper@chipersoft.com>
2015-10-15 14:35:22 -0700

    Add newrelict to gangway

--- src/gangway/index.js
+++ src/gangway/index.js
@@ -3,6 +3,7 @@ var debug           = require('taut.shared/debug')();
 var mq              = require('taut.shared/io/mq');
 var Promise         = require('bluebird');
 var messageHandlers = require('./src/message-handlers');
+var newrelic        = require('newrelic');
 
 module.exports = function () {
 
@@ -10,10 +11,14 @@ module.exports = function () {
 		var args = Array.prototype.slice.call(arguments, 1);
 
 		if (messageHandlers[type]) {
-			return Promise.try(messageHandlers[type], args);
-		} else {
-			debug.error('unknown message type', type, args);
+			return newrelic.createWebTransaction('/digest/' + type, function () {
+				return Promise.try(messageHandlers[type], args).then(function () {
+					newrelic.endTransaction();
+				});
+			});
 		}
+
+		debug.error('unknown message type', type, args);
 	});
 
 };
