9abf70d670c186e0904b778db22096dba9b11e48
ChiperSoft <chiper@chipersoft.com>
2015-10-15 20:38:14 -0700

    Moving newrelic config to bin directory.

--- src/concourse/bin/newrelic.js
+++ src/concourse/bin/newrelic.js
@@ -1,1 +1,25 @@
-../newrelic.js
+/* eslint-disable*/
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['taut-concourse'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '483960bd2582d47f0e268ac0d47bc5e182cc3895',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'trace'
+  }
+}
