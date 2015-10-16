d2b251c463a72e05a153cc7b5ad5ec56b624c707
ChiperSoft <chiper@chipersoft.com>
2015-10-15 20:14:23 -0700

    Nixing newrelic entirely

--- src/gangway/index.js
+++ src/gangway/index.js
@@ -3,19 +3,13 @@ var debug           = require('taut.shared/debug')();
 var mq              = require('taut.shared/io/mq');
 var Promise         = require('bluebird');
 var messageHandlers = require('./src/message-handlers');
-// var newrelic        = require('newrelic');
 
 module.exports = function () {
-
 	return mq.subscribe('irc:incoming', function (type) {
 		var args = Array.prototype.slice.call(arguments, 1);
 
 		if (messageHandlers[type]) {
-			// return newrelic.createBackgroundTransaction('digest:' + type, function () {
-				return Promise.try(messageHandlers[type], args).then(function () {
-					// newrelic.endTransaction();
-				});
-			// }).catch(console.error);
+			return Promise.try(messageHandlers[type], args).catch(console.error);
 		}
 
 		debug.error('unknown message type', type, args);
