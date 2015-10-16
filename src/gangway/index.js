cd5f287d478541e0574af2902730f7d0f6317b17
ChiperSoft <chiper@chipersoft.com>
2015-10-15 19:36:50 -0700

    Disabling gangway newrelic integration until I can figure out wtf is wrong

--- src/gangway/index.js
+++ src/gangway/index.js
@@ -3,7 +3,7 @@ var debug           = require('taut.shared/debug')();
 var mq              = require('taut.shared/io/mq');
 var Promise         = require('bluebird');
 var messageHandlers = require('./src/message-handlers');
-var newrelic        = require('newrelic');
+// var newrelic        = require('newrelic');
 
 module.exports = function () {
 
@@ -11,11 +11,11 @@ module.exports = function () {
 		var args = Array.prototype.slice.call(arguments, 1);
 
 		if (messageHandlers[type]) {
-			return newrelic.createWebTransaction('/digest/' + type, function () {
+			// return newrelic.createBackgroundTransaction('digest:' + type, function () {
 				return Promise.try(messageHandlers[type], args).then(function () {
-					newrelic.endTransaction();
+					// newrelic.endTransaction();
 				});
-			});
+			// }).catch(console.error);
 		}
 
 		debug.error('unknown message type', type, args);
