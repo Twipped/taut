614ef57b3c6e67137212a75e436e199493434b78
ChiperSoft <chiper@chipersoft.com>
2015-10-19 12:14:58 -0700

    Stringify name data before storing in redis

--- src/shared/models/channel/names.js
+++ src/shared/models/channel/names.js
@@ -1,6 +1,7 @@
 
 var redis = require('../../io/redis');
 var indexBy = require('lodash/collection/indexBy');
+var mapValues = require('lodash/object/mapValues');
 
 function key (channel) {
 	return 'channel:' + channel + ':names';
@@ -8,16 +9,19 @@ function key (channel) {
 
 exports.get = function (channel, hashkey) {
 	if (hashkey) {
-		return redis.hget(key(channel), hashkey);
+		return redis.hget(key(channel), hashkey).then(JSON.parse);
 	}
 
-	return redis.hgetall(key());
+	return redis.hgetall(key()).then(function (names) {
+		return mapValues(names, JSON.parse);
+	});
 };
 
 exports.set = function (channel, hashkey, value) {
 	if (typeof hashkey === 'object') {
 		if (Array.isArray(hashkey)) {
 			hashkey = indexBy(hashkey, 'nick');
+			hashkey = mapValues(hashkey, JSON.stringify)
 		}
 		return redis.hmset(key(channel), hashkey);
 	}
