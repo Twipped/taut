95d30387025f4d8f8354e51a9080fcbe41a11b18
ChiperSoft <chiper@chipersoft.com>
2015-10-19 08:39:42 -0700

    Incorrect event name for halted logging.

--- src/tower/src/actions/channelLoggingStopped.js
+++ src/tower/src/actions/channelLoggingStopped.js
@@ -2,8 +2,8 @@
 var mq  = require('taut.shared/io/mq');
 
 module.exports = function (target) {
-	return mq.emit('irc:incoming', 'public', 'logging:halted', target.toLowerCase(), {
-		event: 'logging:halted',
+	return mq.emit('irc:incoming', 'public', 'logging:stopped', target.toLowerCase(), {
+		event: 'logging:stopped',
 		target: target.toLowerCase(),
 		userid: 'TOWER',
 		connid: 'TOWER',
