67d535861db957a584b356fc67fcf933b31189d1
ChiperSoft <chiper@chipersoft.com>
2015-10-20 13:46:13 -0700

    models/channels/topic: Fix crash when loading. Add support for links array

--- src/shared/models/channel/topic.js
+++ src/shared/models/channel/topic.js
@@ -1,5 +1,7 @@
 
 var redis = require('../../io/redis');
+var tryParse = function (input) {try {return JSON.parse(input);} catch (e) {return undefined;}};
+var assign = require('lodash/object/assign');
 
 function key (channel) {
 	return 'channel:' + channel.toLowerCase() + ':topic';
@@ -7,14 +9,27 @@ function key (channel) {
 
 exports.get = function (channel, hashkey) {
 	if (hashkey) {
-		return redis.hget(key(channel), hashkey);
+		if (hashkey === 'links') {
+			return redis.hget(key(channel), hashkey).then(tryParse);
+		}
+
+		return redis.hget(key(channel), hashkey).then(function (data) {
+			if (data.links) data.links = tryParse(data.links);
+			return data;
+		});
 	}
 
-	return redis.hgetall(key());
+	return redis.hgetall(key(channel));
 };
 
 exports.set = function (channel, hashkey, value) {
 	if (typeof hashkey === 'object') {
+
+		if (typeof hashkey.links === 'object') {
+			hashkey = assign({}, hashkey);
+			hashkey.links = JSON.stringify(hashkey.links);
+		}
+
 		return redis.hmset(key(channel), hashkey);
 	}
 
