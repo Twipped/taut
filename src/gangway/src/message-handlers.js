82481e4276a7058d836780adddf4fa243027c6f2
ChiperSoft <chiper@chipersoft.com>
2015-10-23 10:54:13 -0700

    Gangway: Don't count a message as handled until all its IO completes.

--- src/gangway/src/message-handlers.js
+++ src/gangway/src/message-handlers.js
@@ -3,6 +3,7 @@ var debug      = require('taut.shared/debug')('message-handler');
 var pubsub     = require('taut.shared/io/pubsub');
 var irchistory = require('taut.shared/models/system/irc/history');
 var linkify    = require('linkify-it')();
+var Promise    = require('bluebird');
 
 var ChannelTopic = require('taut.shared/models/channel/topic');
 var ChannelNames = require('taut.shared/models/channel/names');
@@ -15,43 +16,50 @@ var trackPublicMessage = require('./message-cache').match;
 var hashPrivateMessage = require('./message-cache').hashPrivateMessage;
 
 exports.system = function (event, data) {
+	debug('system ' + event);
+
 	data.date = new Date(data.timestamp);
 	if (data.message && !data.links) {
 		data.links = linkify.match(data.message);
 	}
 
-	debug('system ' + event);
-	pubsub.channel('irc:system:receive').publish(event, data);
+	var proms = [];
+
+	proms.push(pubsub.channel('irc:system:receive').publish(event, data));
 
 	if (event === 'topic') {
-		ChannelTopic.set(data.target, {
+		proms.push(ChannelTopic.set(data.target, {
 			message: data.message,
 			links: data.links
-		});
+		}).catch(debug.error));
 	}
 
 	if (event === 'topic:time') {
-		ChannelTopic.set(data.target, {
+		proms.push(ChannelTopic.set(data.target, {
 			nick: data.nick,
 			hostmask: data.hostmask,
 			date: data.time
-		});
+		}).catch(debug.error));
 	}
 
 	if (event === 'topic:url') {
-		ChannelTopic.set(data.target, 'url', data.url);
+		proms.push(ChannelTopic.set(data.target, 'url', data.url).catch(debug.error));
 	}
 
 	if (event === 'names') {
-		ChannelNames.set(data.target, data.names);
+		proms.push(ChannelNames.set(data.target, data.names).catch(debug.error));
 	}
 
 	if (event === 'mode:channel') {
-		ChannelModes.set(data.target, data.modes);
+		proms.push(ChannelModes.set(data.target, data.modes).catch(debug.error));
 	}
+
+	return Promise.all(proms);
 };
 
 exports.private = function (event, userid, data) {
+	debug('private ' + event, userid);
+
 	data.hash = hashPrivateMessage(data);
 	data.date = new Date(data.timestamp);
 
@@ -59,15 +67,20 @@ exports.private = function (event, userid, data) {
 		data.links = linkify.match(data.message);
 	}
 
-	debug('private ' + event, userid);
+	var proms = [];
 
-	pubsub.channel('irc:user:' + userid + ':receive').publish(event, data);
 
-	return irchistory.pushPrivate(data);
+	proms.push(pubsub.channel('irc:user:' + userid + ':receive').publish(event, data));
+
+	proms.push(irchistory.pushPrivate(data));
+
+	return Promise.all(proms);
 };
 
 exports.public = function (event, channel, data) {
 	channel = channel.toLowerCase();
+
+	debug('public ' + event, channel);
 
 	var isNewMessage = trackPublicMessage(data);
 	if (!isNewMessage) {
@@ -79,29 +92,33 @@ exports.public = function (event, channel, data) {
 		data.links = linkify.match(data.message);
 	}
 
-	pubsub.channel('irc:public:' + channel + ':receive').publish(event, data);
+	var proms = [];
+
+	proms.push(pubsub.channel('irc:public:' + channel + ':receive').publish(event, data));
 
 	if (event === 'topic') {
-		ChannelTopic.set(data.target, {
+		proms.push(ChannelTopic.set(data.target, {
 			nick: data.nick,
 			hostmask: data.hostmask,
 			message: data.message,
 			date: new Date()
-		});
-		pubsub.channel('irc:public:' + channel + ':receive').publish(event, data);
+		}).catch(debug.error));
+		proms.push(pubsub.channel('irc:public:' + channel + ':receive').publish(event, data));
 	}
 
 	if (event === 'join') {
-		ChannelNames.add(data.target, data.nick, { 'nick':data.nick,'op':false,'halfop':false,'voice':false });
-		pubsub.channel('irc:public:' + channel + ':receive').publish('names:add', data);
+		proms.push(ChannelNames.add(data.target, data.nick, { 'nick':data.nick }).catch(debug.error));
+		proms.push(pubsub.channel('irc:public:' + channel + ':receive').publish('names:add', data));
 	}
 
 	if (event === 'part' || event === 'quit:channel' || event === 'kick') {
-		ChannelNames.remove(data.target, data.nick);
-		pubsub.channel('irc:public:' + channel + ':receive').publish('names:remove', data);
+		proms.push(ChannelNames.remove(data.target, data.nick).catch(debug.error));
+		proms.push(pubsub.channel('irc:public:' + channel + ':receive').publish('names:remove', data));
 	}
 
-	debug('public ' + event, channel);
-	return irchistory.pushPublic(data);
+
+	proms.push(irchistory.pushPublic(data));
+
+	return Promise.all(proms);
 };
 
