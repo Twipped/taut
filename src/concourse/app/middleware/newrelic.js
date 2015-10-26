b4d30cd4c7a8482cce2fdaabbf1975795199b79d
ChiperSoft <chiper@chipersoft.com>
2015-10-26 10:56:53 -0700

    Removing newrelic integration

--- src/concourse/app/middleware/newrelic.js
+++
@@ -1,9 +0,0 @@
-var newrelic = require('newrelic');
-
-module.exports = function (name) {
-	return function (req, res, next) {
-		if (name) {newrelic.setTransactionName(name);}
-		res.locals.newrelic = newrelic.getBrowserTimingHeader();
-		next();
-	};
-};
--- src/concourse/app/routes/index.js
+++ src/concourse/app/routes/index.js
@@ -4,10 +4,10 @@ var express = require('express');
 var router = express.Router(); // eslint-disable-line new-cap
 
 /* GET home page. */
-router.get('/', require('../middleware/newrelic')('frontpage'), function (req, res) {
+router.get('/', function (req, res) {
 	res.render('index.hbs', { title: 'Freenode IRC Network Nexus' });
 });
 
-router.use('/c', require('../middleware/newrelic')('channel'), require('./c'));
+router.use('/c', require('./c'));
 
 module.exports = router;
