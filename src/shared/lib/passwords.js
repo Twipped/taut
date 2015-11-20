178cf59ff3936bac374dce6be5370e9039af727c
ChiperSoft <chiper@chipersoft.com>
2015-11-20 12:22:40 -0800

    Shared/passwords: Don't try to encrypt/decrypt empty strings.

--- src/shared/lib/passwords.js
+++ src/shared/lib/passwords.js
@@ -47,11 +47,13 @@ exports.check = function (candidatePassword, hash, cb) {
 };
 
 exports.encrypt = function (key, original) {
+	if (!original) return '';
 	var cipher = crypto.createCipher('aes-256-cbc', key);
 	return cipher.update(original, 'utf8', 'base64') + cipher.final('base64');
 };
 
 exports.decrypt = function (key, encrypted) {
+	if (!encrypted) return '';
 	var decipher = crypto.createDecipher('aes-256-cbc', key);
 	return decipher.update(encrypted, 'base64', 'utf8') + decipher.final('utf8');
 };
